e2945f7df2514e38f97871146d7fbd02870cec3d
phamhoan_nuce <phamhoanxda@gmail.com>
2020-09-15 00:15:25 +0700

    add 1509

--- js/custom.js
+++ js/custom.js
@@ -47,3 +47,58 @@ $(document).ready(function () {
     autoplayHoverPause: true,
   });
 });
+/* =====================================
+             STATS
+======================================*/
+
+$(document).ready(function () {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 2500,
+  });
+});
+
+/* =====================================
+             CLIENTS
+======================================*/
+
+$(document).ready(function () {
+  $("#clients-list").owlCarousel({
+    items: 6,
+    autoplay: true,
+    smartspeed: 350,
+    loop: true,
+    autoplayHoverPause: true,
+  });
+});
+
+/* ======================================
+                 NAVIGATION BAR 
+========================================= */
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 50) {
+      $("nav").removeClass("vesco-top-nav");
+      $("#back-to-top").fadeOut();
+    } else {
+      $("nav").addClass("vesco-top-nav");
+      $("#back-to-top").fadeIn();
+    }
+  });
+});
+
+//smooth scroll
+$(function () {
+  $("a.smooth-scroll").click(function (event) {
+    event.preventDefault();
+    //get anf return id like #work, #about
+    var section = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(section).offset().top - 64,
+      },
+      1250,
+      "easeInOutExpo"
+    );
+  });
+});
